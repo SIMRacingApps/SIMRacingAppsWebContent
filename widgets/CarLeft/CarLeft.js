d96536328ab00eb3cb1b6b0441dfbdc1e840f7ae
jfgilliam@yahoo.com <jfgilliam@yahoo.com>
2017-12-26 21:43:09 -0500

    1. Closed #122 (https://github.com/SIMRacingApps/SIMRacingApps/issues/122)        CarLeft, CarRight: Show when car(s) are to the left or right of you        Added 2 new widgets, CarLeft and CarRight.        These are simply arrows that follow the spotter indicators.        There are 2 sets of them, so you can place 2 lefts in different places.        Use the transparent feature of Electron, to hide them when they're not showing.
        2. Added new API call to get the spotter messages.
               /Car/(CARIDENTIFIER)/SpotterMessage
           Can return the following
              - (blank) = not talking
              - CLEAR = no cars around you
              - CARLEFT = car to your left
              - CARRIGHT = car to your right
              - CARLEFTRIGHT = car on your left and right
              - CARSLEFT = 2 or more cars on your left
              - CARSRIGHT = 2 or more cars on your right 
        
        3. Fixed #131
    (https://github.com/SIMRacingApps/SIMRacingApps/issues/131)
           Java 9 support.

---
+++ widgets/CarLeft/CarLeft.js
@@ -0,0 +1,82 @@
+'use strict';
+/**
+ * This widget displays when car(s) are to the left of you
+ * <p>
+ * Example(s):
+ * <p><b>
+ * &lt;sra-car-left &gt;&lt;/sra-car-left&gt;<br />
+ * </b>
+ * <img src="../widgets/CarLeft/icon.png" />
+ * @ngdoc directive
+ * @name sra-car-left
+ * @param {integer} data-sra-args-interval The interval, in milliseconds, that this widget will update from the server. Default is 20.
+ * @author Jeffrey Gilliam
+ * @since 1.6
+ * @copyright Copyright (C) 2015 - 2017 Jeffrey Gilliam
+ * @license Apache License 2.0
+ */
+define(['SIMRacingApps','css!widgets/CarLeft/CarLeft'],
+function(SIMRacingApps) {
+
+    var self = {
+        name:            "sraCarLeft",
+        url:             'CarLeft',
+        template:        'CarLeft.html',
+        defaultWidth:    300,
+        defaultHeight:   300,
+        defaultInterval: 20   //initialize with the default interval
+    };
+
+    self.module = angular.module('SIMRacingApps'); //get the main module
+
+    self.module.directive(self.name,
+           ['sraDispatcher', '$filter','$rootScope',
+    function(sraDispatcher,   $filter,  $rootScope) {
+        return {
+            restrict: 'EA',
+            scope: true,
+            templateUrl: sraDispatcher.getWidgetUrl(self.url) + '/' + self.template,
+            controller: [ '$scope', function($scope) {
+                $scope.directiveName   = self.name;
+                $scope.defaultWidth    = self.defaultWidth;
+                $scope.defaultHeight   = self.defaultHeight;
+                $scope.defaultInterval = self.defaultInterval;
+
+                $scope.arrow = "";
+            }]
+            , link: function($scope,$element,$attrs) {
+                //copy arguments to our $scope
+                //$scope.value = $scope[self.name] = $attrs[self.name] || $attrs.sraArgsCar || $attrs.sraArgsCarNumber || "REFERENCE";
+                $scope.value = 
+                $scope[self.name] = sraDispatcher.getTruthy($scope.sraArgsCAR, $attrs.sraArgsCar, $attrs.sraArgsCarNumber, $scope.sraArgsVALUE, $attrs[self.name], $attrs.sraArgsValue, "REFERENCE");
+
+
+                //removed these attributes from the template because sraCar was not resolved at the time of compilation
+                $attrs.sraArgsData =
+                    ";Car/" + $scope.value + "/SpotterMessage";                
+
+                $scope.$watch("data.Car['"+$scope.value+"'].SpotterMessage.Value", function() {
+                    if ($scope.data.Car[$scope.value].SpotterMessage.Value == "CARLEFT"
+                    ||  $scope.data.Car[$scope.value].SpotterMessage.Value == "CARLEFTRIGHT") {
+                        $scope.arrow = "&lt;";
+                    }
+                    else
+                    if ($scope.data.Car[$scope.value].SpotterMessage.Value == "CARSLEFT") {
+                        $scope.arrow = "&lt;&lt;";
+                    }
+                    else {
+                        $scope.arrow = "";
+                    }
+                });
+
+            /**standard code that should be in every directive **/
+                $rootScope.$on('sraResize', sraDispatcher.resize($scope,$element,self.defaultWidth,self.defaultHeight));
+
+                $scope.names = sraDispatcher.subscribe($scope,$attrs,self.defaultInterval); //register subscriptions and options to the dispatcher
+
+            }
+        };
+    }]);
+
+    return self;
+});
