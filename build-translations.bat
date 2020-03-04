2ccfaf1bd99e3c73367d81d72ec7d5a1a04b2dc8
jfgilliam@yahoo.com <jfgilliam@yahoo.com>
2020-03-03 20:16:44 -0500

    added Spanish files for translating

--- build-translations.bat
+++ build-translations.bat
@@ -1,5 +1,6 @@
 @echo on
-set LANG=de
-del %LANG%.zip
-..\..\..\..\PortableApps\7-ZipPortable\App\7-Zip64\7z a -tzip -r %LANG% listing.json *-%LANG%.properties *-%LANG%.json
+set LANG=%1
+del %TMP%\%LANG%.zip
+7z a -bb1 -tzip -r %TMP%\%LANG% listing.json *-%LANG%*.properties *-%LANG%*.json
+dir %TMP%\%LANG%.zip
 pause
